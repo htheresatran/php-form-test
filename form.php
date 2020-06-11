583054a0ba35b5a02c93f6e2dc8f1e19b4055375
htheresatran <63614161+htheresatran@users.noreply.github.com>
2020-06-11 11:01:31 -0700

    Create form.php

---
+++ form.php
@@ -0,0 +1,14 @@
+<?php
+if(){//show data
+
+}else{//show form
+  echo 
+  ' 
+  <form action="">
+  <p>First Name <input type="text" name="FirstName" /></p>
+  <p><input type="submit" /></p>
+  </form>
+  
+  ';
+}
+?>
