ed100576905ec26eb62ece3673eb7fdcc92b5465
htheresatran <63614161+htheresatran@users.noreply.github.com>
2020-06-12 10:59:44 -0700

    Update form.php

--- form.php
+++ form.php
@@ -1,14 +1,13 @@
 <?php
 if(isset($_GET["FirstName"])){//show data
-    echo  $_GET["FirstName"];
+  echo $_GET["FirstName"];
 }else{//show form
   echo 
-  ' 
+  '
   <form action="">
-  <p>First Name <input type="text" name="FirstName" /></p>
+  <P>First Name <input type="text" name="FirstName" /></p>
   <p><input type="submit" /></p>
   </form>
-  
   ';
 }
 ?>
